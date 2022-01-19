438742df85f918040e1a5d26d8796ba0be05b910
Higor Carneiro <higor12cs@gmail.com>
2022-01-19 00:17:50 -0300

    Atualizando remocao de arquivos, README.md, requirements.txt, geracao de .log / Incluindo status indicadores

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # whatspy
-Automatizando o envio de mensagens no WhatsApp usando Python
+Usando Python para automatizar o envio de mensagens no WhatsApp de modo oculto.
+
+## Objetivo
+Basicamente meu objetivo era automatizar o envio de imagens armazenadas no Excel,  tudo isso de forma oculta, para que não atrapalhe o uso do computador no tempo de execução do script.
+
+# Configurações Iniciais
+Nesse programa eu automatizei o envio de imagens no WhatsApp usando o Excel para obter os dados. Dentro do arquivo .xls eu tenho uma lista de contatos, e ao lado de cada contato eu tenho o nome da figura que será enviada, tudo na mesma planilha. 
+
+Dito isso, o programa precisa dos seguintes dados:
+
+## Caminho do GeckoDriver
+
+Isso já está pré-definido dentro do código como: `driver_path = str(Path(__file__).parent.absolute()) + r"\geckodriver.exe"`
+
+Mas caso precise alterar o esse caminho por efeitos de atualizações ou outras necessidades, essa é a variável a ser alterada.
+
+**Observação:**
+
+Deixei uma cópia do GeckoDriver dentro da pasta código, até a data dessa publicação, essa versão estava funcionado perfeitamente, tentarei manter esse repositório sempre atualizado para demais situações.
+
+## Caminho do perfil no Firefox
+Aqui é onde fixamos um perfil do navegador que o código vai usar sempre. Isso dribla a necessidade de inserir o QR code do WhatsAppWeb toda vez que o programa rodar. Automatizar esse processo foi um pouco difícil, tentei várias vezes com o Chrome, mas a minha ultima solução foi o Firefox. 
+
+Fazer essa configuração é simples, primeiro você instala o Firefox normalmente. Feito isso, pressione as teclas `Windows + R` e digite `firefox.exe -p` Isso ira abrir o gerenciador de perfis do Firefox. Crie um novo perfil com o nome de sua preferencia e inicie o navegador, deixe a opção `Usar o perfil selecionado sem perguntar ao iniciar` marcada. Após abrir o navegador, faça login no WhatsAppWeb normalmente.
+
+Agora, pressione novamente `Windows + R` e digite `%appdata%`. Dentro da pasta `\Mozilla\Firefox\Profiles` você ira encontrar uma pasta com o nome de perfil que voce acabou de criar. Entre nela e copie o caminho e substitua a variável `profile_path` no código. Ela ficara mais ou menos assim: `profile_path = r"C:\Users\<seu_user>\AppData\Roaming\Mozilla\Firefox\Profiles\<seu_profile>"`
+
+## Caminho do arquivo Excel
+Dentro do código nós temos a variável: `planilha_contatos = str(Path(__file__).parent.absolute()) + r"\<sua_planilha>.xlsx"` Essa variável aponta pra um arquivo .xls dentro da mesma pasta do código, sinta-se a vontade para alterar o caminho até o seu arquivo, mas para manter isso simples, é só colocar a sua planilha no mesmo diretório e trocar o `<sua_planilha>.xlsx` pelo nome do seu arquivo Excel.
+
+Agora precisamos indicar em qual "aba" o programa ira procurar pelos dados. Então seguindo a mesma metodologia de antes, substitua o atributo da variável `aba_planilha` pelo nome da sua aba dentro da sua planilha. 
+
+Essa variável deve ficar parecida com isso: `aba_planilha = "<aba>"`
+
+## Guiando o programa dentro do arquivo do Excel
+Aqui iremos indicar para o programa onde começar a ler o nome das figuras e dos contatos que serão usados para enviar as imagens. Essa parte funciona mais ou menos assim: 
+|-|*ColunaA*|*ColunaB*|
+|--|--|--|
+|***Linha1*** |**InicioContatos**|**InicioFiguras**|
+|***Linha2***| Contato1 | Figura1 |
+|***Linha3***| Contato2 | Figura2 |
+|***Linha4***| Contato3 | Figura3 |
+|***Linha5***| | |
+
+Pegando a planilha acima de exemplo, nossa lista de contatos começa na coluna A, na linha 2. Com isso, informamos essas coordenadas para o código e ele seguira lendo essa coluna até achar um espaço vazio, esse espaço vazio definirá o fim da lista. 
+
+O mesmo acontece com as figuras, que por sua vez, começam na coluna B e na linha 2.
+
+O nosso programa entende o número de linhas perfeitamente, mas não entende as colunas como letras, e sim, como números também. Então nesse caso, substituímos a coluna A por 1, a coluna B por 2 e assim por diante.
+
+E então, dentro do código, devemos atribuir as variáveis `coluna_contatos`, `linha_contatos`, `coluna_figuras`, `linha_figuras` seus respectivos valores.
+
+Exemplo:
+
+    coluna_contatos = 1  #coluna A
+    linha_contatos = 2  #linha 2
+    coluna_figuras = 2  #coluna B
+    linha_figuras = 2  #linha 2
+
+Observação:
+
+O número de figuras deve ser correspondente ao número de contatos, e visse versa. Caso não seja o caso, encontraremos erros no código.
+
+## Rodando o código
+Para rodar o código é simples, basta executar o arquivo e aguardar sua execução terminar. O código também gera um arquivo `historico.log` que guarda todo o log do processo. 
+
+# É isso!
+Sei que meu código está longe de ser bom, mas ainda estou aperfeiçoando, e também é bom lembrar que sou apenas um iniciante em Python. 
+Qualquer dúvida ou sugestão é só entrar em contato. Obrigado!
--- codigo/firefox.py
+++ codigo/firefox.py
@@ -1,78 +1,22 @@
-import os, time
+import os, time, pyperclip, logging, shutil
 from pathlib import Path
 
-from openpyxl import Workbook
 from openpyxl import load_workbook
-
 from PIL import ImageGrab
 import win32com.client as win32
 
-from selenium import webdriver
 from selenium.webdriver import Firefox
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.keys import Keys 
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from prettytable import PrettyTable
 
-import logging
-
-start_time = datetime.now()
-
-logging.basicConfig(filename=str(Path(__file__).parent.absolute()) + "\\" + "historico.log", level=logging.INFO, format='%(message)s')
-logging.info("")
-logging.info("==================== INICIO ====================")
-logging.info("")
-logging.info("Começo do Script: " + str(datetime.now().strftime(r'%Y-%m-%d %H:%M:%S')))
-logging.info("")
-
-#=== CRIANDO UM PROFILE NO FIREFOX ===#
-
-# Windows + R
-# "firefox.exe -p"
-# Crie um novo perfil
-# Entre na pasta: "%appdata%\Roaming\Mozilla\Firefox\Profiles"
-# Copie o caminho para a variavel "profile_path"
-
-lista_contatos = []
-lista_figuras = []
-
-    #=== CONFIGURACOES ===#
-
-#=== CAMINHO DO WEBDRIVER E DA PASTA PROFILE DO FIREFOX ===#
-driver_path = str(Path(__file__).parent.absolute()) + r"\geckodriver.exe" #caminho do navegador
-profile_path = r"C:\Users\<seu_user>\AppData\Roaming\Mozilla\Firefox\Profiles\<seu_profile>" #caminho do profile, geralmente localizado em "%appdata%\Roaming\Mozilla\Firefox\Profiles" *fazer login no whatsapp antes
-
-#=== NOME DA PLANILHA USADA PARA RODAR O ENVIO ===#
-planilha_contatos = str(Path(__file__).parent.absolute()) + r"\<sua_planilha>.xlsx" #nessa planilha os contatos ficam a esquerda e os nomes das imagens ficam a direita, tudo na mesma planilha
-aba_planilha = "<aba>"
-
-#=== VARIAVEIS P/ DEFINIR ONDE COMEÇAM AS LISTAS DE CONTATOS E FIGURAS ===#
-
-coluna_contatos = 1 #coluna A
-linha_contatos = 2 #linha 2
-coluna_figuras = 2 #coluna B
-linha_figuras = 2 #linha 2
-
-    #=== INICIANDO O NAVEGADOR ===#
-options = Options()
-options.headless = False #true pra rodar o firefox em modo oculto
-options.add_argument("-profile") 
-options.add_argument(profile_path) #adicionando a pasta profile p/ o whatsapp não ficar pedindo o QR code  
-
-service = Service(driver_path) #caminho do driver
-
-driver = Firefox(service=service, options=options) 
-
-driver.get("http://web.whatsapp.com")
-logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Abrindo driver no WhatsApp")
-print("Abrindo driver no WhatsApp")
-
-wait = WebDriverWait(driver, 800)
-print("Wait...")
-logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Wait...")
+start_time = datetime.now() #iniciando timer do script
 
 def importar_contatos(planilha):
     print("Importando contatos...")
@@ -80,7 +24,6 @@ def importar_contatos(planilha):
     wb = load_workbook(planilha, read_only=True, keep_vba=True)
 
     try:
-        print("Importando contatos...")
         ws = wb[aba_planilha] #nome da aba da planilha
         
         for row in range(linha_contatos, ws.max_row+1): #numero da linha onde começa / maximo de linhas +1 para não finalizar antes do ultimo elemento   
@@ -113,6 +56,25 @@ def importar_figura(planilha):
     finally:
         wb.close() #fecha o workbook p/ win32com conseguir copiar a imagem
 
+def importar_datas(planilha):
+    print("Importando datas...")
+    logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Importando datas...")
+    wb = load_workbook(planilha, read_only=True, keep_vba=True, data_only=True)
+
+    try:
+        ws = wb[aba_planilha] #nome da aba da planilha
+    
+        for row in range(linha_datas, ws.max_row+1): #numero da linha onde começa / maximo de linhas +1 para não finalizar antes do ultimo elemento
+            if(ws.cell(row, coluna_datas).value is None): #se valor é nulo -> break
+                break
+            else:
+                lista_datas.append(ws.cell(row, coluna_datas).value.strftime(r"%d/%m/%Y %H:%M"))
+    except Exception as e:
+        print("Erro no modulo 'importar_datas': " + str(e))
+        logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Erro no modulo 'importar_datas': " + str(e))
+    finally:
+        wb.close() #fecha o workbook p/ win32com conseguir copiar a imagem
+
 def salvar_imagem(planilha):
     print("Salvando imagens...")
     logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Salvando imagens...")
@@ -120,6 +82,14 @@ def salvar_imagem(planilha):
     wb = excel.Workbooks.Open(planilha)
 
     try:
+        MYDIR = str(Path(__file__).parent.absolute()) + "\\temp"
+        CHECK_FOLDER = os.path.isdir(MYDIR)
+
+        if not CHECK_FOLDER:
+            os.makedirs(MYDIR)
+            print("Pasta 'temp' criada.")
+            logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Pasta 'temp' criada.")
+
         sheet = excel.Sheets(aba_planilha)
 
         for i, shape in enumerate(sheet.Shapes): #percorre todos os shapes da pasta selecionada
@@ -128,7 +98,7 @@ def salvar_imagem(planilha):
                     shape.Copy()
                     image = ImageGrab.grabclipboard() #copia
                     image = image.convert('RGB') #converte de RGBA p/ RGB
-                    image.save(str(Path(__file__).parent.absolute()) + "\\" + figura + ".jpg", "jpeg") #salva com a extensao .jpg
+                    image.save(str(Path(__file__).parent.absolute()) + "\\temp\\" + figura + ".jpg", "jpeg") #salva com a extensao .jpg
     except Exception as e:
         print("Erro no modulo 'salvar_imagem': " + str(e))
         logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Erro no modulo 'salvar_imagem': " + str(e))
@@ -137,13 +107,18 @@ def salvar_imagem(planilha):
         wb.Close(False) #fecha o workbook
         excel.Quit()
 
-def envia_imagens(contatos, imagens):
+def envia_imagens(contatos, imagens, datas):
     print("Enviando mensagens...")
     logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Enviando mensagens...")
     
-    time.sleep(5)
     try:
         for i in range(0, len(contatos)): #percorre do 0 até o tamanho do array dos contatos enviando para o contato a respectiva imagem na mesma linha da planilha
+            #verificando data de atualizacao dos indicadores
+            if datetime.strptime(lista_datas[i], r"%d/%m/%Y %H:%M") < datetime.today() - timedelta(days=1):
+                table.add_row([imagens[i], datas[i]])
+                #print(lista_datas[i])
+                #print(datetime.strptime(lista_datas[i], r"%d/%m/%Y %H:%M"))
+
             print("Enviando imagem " + str(i+1) + ": ## " + str(imagens[i]) + " ## Para: ## " + str(contatos[i]) + " ##")
             logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Enviando imagem " + str(i+1) + ": ## " + str(imagens[i]) + " ## Para: ## " + str(contatos[i]) + " ##")
             #procura na pagina o elemento com o mesmo nome do contato
@@ -153,34 +128,68 @@ def envia_imagens(contatos, imagens):
             group_title.click()
             #procura na pagina o elemento de anexo de arquivos
             driver.find_element(By.CSS_SELECTOR, "span[data-icon='clip']").click()
+            time.sleep(1)
             attach = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
             #passa o caminho da imagem para ser anexada
-            attach.send_keys(str(Path(__file__).parent.absolute()) + "\\" + str(imagens[i]) + ".jpg")
-            time.sleep(3)
+            attach.send_keys(str(Path(__file__).parent.absolute()) + "\\temp\\" + str(imagens[i]) + ".jpg")
+            time.sleep(1)
             #tecla enviar
             send = driver.find_element(By.CSS_SELECTOR, "span[data-icon='send']")
             send.click()
-            time.sleep(5)
+            time.sleep(1)
     except Exception as e:
         print("Erro no modulo 'envia_imagens': " + str(e))
         logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Erro no modulo 'envia_imagens': " + str(e))
-    finally:
-        time.sleep(3)
-        driver.close() #fecha o driver
+
+def envia_tabelaAtrasos(ptablle, contatos):
+    try:
+        for i in range(0, len(contatos)):
+            print("Enviando resultados...")
+            logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Enviando resultados...")
+            
+            x_arg = '//span[contains(@title, ' + '"' + str(contatos[i]) + '"' + ')]'
+            group_title = wait.until(EC.presence_of_element_located((
+                By.XPATH, x_arg)))
+            group_title.click()
+
+            #procura na pagina o elemento de anexo de arquivos
+            input_path = '//div[@contenteditable="true"][@data-tab="10"]'
+            input_box = wait.until(EC.presence_of_element_located((
+                By.XPATH, input_path)))
+            
+            mensagem = "Indicadores desatualizados:\n" + "```" + str(ptablle) + "```"
+            
+            pyperclip.copy(mensagem)
+            input_box.send_keys(Keys.CONTROL + "V")
+            input_box.send_keys(Keys.ENTER)
+            pyperclip.copy("")
+    except Exception as e:
+        print("Erro no modulo 'envia_tabelaAtrasos': " + str(e))
+        logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Erro no modulo 'envia_tabelaAtrasos': " + str(e))
 
 def deleta_arquivos():
-    for figura in lista_figuras:
-        try:
-            logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Deletando arquivo: " + str(Path(__file__).parent.absolute()) + "\\" + str(figura) + ".jpg")
-            os.remove(str(Path(__file__).parent.absolute()) + "\\" + str(figura) + ".jpg")   
-        except Exception as e:
-            print("Erro no modulo 'deleta_arquivos': " + str(e))
-            logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Erro no modulo 'deleta_arquivos': " + str(e))
+    #deletando pasta temp de imagens
+    try: 
+        shutil.rmtree(str(Path(__file__).parent.absolute()) + "\\temp")
+        print("Pasta 'temp' encontrada e deletada.")
+        logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Pasta 'temp' encontrada e deletada.")
+    except Exception as e:
+        print("Pasta 'temp' não encontrada: " + str(e))
+        logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Pasta 'temp' não encontrada: " + str(e))
+
+    #deletando pasta %temp%/gen_py
+    try: 
+        shutil.rmtree(os.path.expandvars(r'%TEMP%\gen_py'))
+        print("Pasta 'gen_py' encontrada e deletada.")
+        logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Pasta 'gen_py' encontrada e deletada.")
+    except Exception as e:
+        print("Pasta 'gen_py' não encontrada: " + str(e))
+        logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Pasta 'gen_py' não encontrada: " + str(e))
 
 def rotina():
     #=== EXECUTANDO FUNÇÕES ===#
     #importando contatos
-    importar_contatos(planilha_contatos) #passando a planilha carregada antes como argumento
+    importar_contatos(planilha_contatos) 
     print("Lista de contatos importada:") 
     print(lista_contatos)
     print("### " + str(len(lista_contatos)) + " Contatos importados ###")
@@ -189,7 +198,7 @@ def rotina():
     logging.info(lista_contatos)
 
     #importando figuras
-    importar_figura(planilha_contatos) #passando a planilha carregada antes como argumento
+    importar_figura(planilha_contatos)
     print("Lista de figuras importadas:")
     print(lista_figuras)
     print("### " + str(len(lista_figuras)) + " Figuras importadas ###")
@@ -197,12 +206,79 @@ def rotina():
     logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === ### " + str(len(lista_figuras)) + " Figuras importadas ###")
     logging.info(lista_figuras)
 
-    salvar_imagem(planilha_contatos) #passando a planilha carregada antes como argumento
+    #importando datas
+    importar_datas(planilha_contatos) 
+    print("Lista de datas importadas:")
+    print(lista_datas)
+    print("### " + str(len(lista_datas)) + " Datas importadas ###")
+    logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Lista de datas importadas:")
+    logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === ### " + str(len(lista_datas)) + " Datas importadas ###")
+    logging.info(lista_datas)
 
-    envia_imagens(lista_contatos, lista_figuras) #passa a lista de contatos e a lista de figuras armazenadas nos arrays como argumentos
+    salvar_imagem(planilha_contatos) 
+    time.sleep(5)
+    envia_imagens(lista_contatos, lista_figuras, lista_datas) 
+    time.sleep(5)
+    envia_tabelaAtrasos(table, contatos_adm)
+    deleta_arquivos()
 
-    #excluindo as figuras geradas
-    deleta_arquivos()  
+#=== INICIANDO LOG ===#
+logging.basicConfig(filename=str(Path(__file__).parent.absolute()) + "\\" + "historico.log", level=logging.INFO, format='%(message)s')
+logging.info("")
+logging.info("*** INICIO - " + str(datetime.now().strftime(r'%Y-%m-%d %H:%M:%S')))
+
+#=== CRIANDO UM PROFILE NO FIREFOX ===#
+
+# Windows + R
+# "firefox.exe -p"
+# Crie um novo perfil
+# Entre na pasta: "%appdata%\Roaming\Mozilla\Firefox\Profiles"
+# Copie o caminho para a variavel "profile_path"
+
+lista_contatos = []
+lista_figuras = []
+lista_datas = []
+
+contatos_adm = ["<contatos_adm>"]
+
+#=== CONFIGURACOES ===#
+
+#=== CAMINHO DO WEBDRIVER E DA PASTA PROFILE DO FIREFOX ===#
+driver_path = str(Path(__file__).parent.absolute()) + r"\geckodriver.exe" #caminho do navegador
+profile_path = r"C:\Users\<seu_usuario>\AppData\Roaming\Mozilla\Firefox\Profiles\<seu_profile>" #caminho do profile, geralmente localizado em "%appdata%\Roaming\Mozilla\Firefox\Profiles" *fazer login no whatsapp antes
+
+#=== NOME DA PLANILHA USADA PARA RODAR O ENVIO ===#
+planilha_contatos = str(Path(__file__).parent.absolute()) + r"\<sua_planilha>" #nessa planilha os contatos ficam a esquerda e os nomes das imagens ficam a direita, tudo na mesma planilha
+aba_planilha = "<sua_aba>"
+
+#=== VARIAVEIS P/ DEFINIR ONDE COMEÇAM AS LISTAS DE CONTATOS E FIGURAS ===#
+
+coluna_contatos = 1 
+linha_contatos = 1 
+coluna_figuras = 1 
+linha_figuras = 1
+coluna_datas = 1
+linha_datas = 1
+
+#=== INICIANDO A TABELA DE RESULTADOS ===#
+table = PrettyTable(["INDICADOR", "DATA ULT AT"])
+
+#=== INICIANDO O NAVEGADOR ===#
+options = Options()
+options.headless = False #true pra rodar o firefox em modo oculto
+options.add_argument("-profile") 
+options.add_argument(profile_path) #adicionando a pasta profile p/ o whatsapp não ficar pedindo o QR code  
+
+service = Service(driver_path) #caminho do driver
+
+driver = Firefox(service=service, options=options) 
+
+driver.get("http://web.whatsapp.com")
+logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Abrindo driver no WhatsApp")
+print("Abrindo driver no WhatsApp")
+
+wait = WebDriverWait(driver, 800)
+logging.info(str(datetime.now().strftime(r'%H:%M:%S')) + " === Wait...")  
 
 #=== VERIFICANDO SE EXISTE O ARQUIVO EXCEL ===#
 
@@ -213,19 +289,17 @@ if os.path.exists(planilha_contatos):
 else:
     print("Arquivo Excel nao existe!")
     logging.error(str(datetime.now().strftime(r'%H:%M:%S')) + " === Arquivo Excel nao existe! Verifique se '" + planilha_contatos + "' esta correto.")
-    time.sleep(3)
     driver.close() #fecha o driver
 
 end_time = datetime.now()
 
-#logging final e tempo de execucao
-logging.info("")
-logging.info("Final do Script: " + str(datetime.now().strftime(r'%Y-%m-%d %H:%M:%S')))
-logging.info("Tempo de execução: {}".format(end_time - start_time))
-print('---- Tempo de execução: {} ----'.format(end_time - start_time))
+time.sleep(5)
+driver.close()
 
-logging.info("")
-logging.info("====================== FIM ======================")
-logging.info("")
+#logging final e tempo de execucao
+logging.info("*** Tempo de execução: {}".format(end_time - start_time))
+print('*** Tempo de execução: {}'.format(end_time - start_time))
+
+logging.info("*** FIM *** - " + str(datetime.now().strftime(r'%Y-%m-%d %H:%M:%S')))
 
 print(str(Path(__file__).parent.absolute()))
